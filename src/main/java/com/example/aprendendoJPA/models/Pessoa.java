08072ab37d0e874bedf39257701a574af002d928
valquiria matter <matter.valquiria@gmail.com>
2022-06-30 09:49:11 -0300

    add ifuncionalidades

--- src/main/java/com/example/aprendendoJPA/models/Pessoa.java
+++ src/main/java/com/example/aprendendoJPA/models/Pessoa.java
@@ -1,15 +1,15 @@
 package com.example.aprendendoJPA.models;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
-import javax.persistence.Id;
 import java.util.List;
 
 @Entity
@@ -20,7 +20,7 @@ public class Pessoa {
         private Long id;
         private String nome;
 
-        @OneToOne //Definindo o Relacionamento Um para Um
+        @OneToOne(fetch = FetchType.LAZY) //Definindo o Relacionamento Um para Um com o comportamento LAZY(preguiçoso)
         @JoinColumn(name = "fk_id_endereco") //Nomeando a FK
         private Endereco endereco;
 
@@ -28,7 +28,7 @@ public class Pessoa {
         //UNIDIRECIONAL -> @JoinColumn(name = "pessoa_id") //Cria uma FK em Cachorros para Referenciar o Id da Pessoa e Evitar uma Terceira Tabela com o Relacionamento
         private List<Cachorro> cachorros;
 
-        @ManyToMany //Definindo o Relcionamento Muitos para Muitos
+        @ManyToMany//Definindo o Relcionamento Muitos para Muitos
         @JoinTable(name = "trabalhos_pessoas") //Marca Pessoa como Dona do Relacionamento e Define Informações da Coluna de União do Relacionamento
         private List<Trabalho> trabalhos;
 
