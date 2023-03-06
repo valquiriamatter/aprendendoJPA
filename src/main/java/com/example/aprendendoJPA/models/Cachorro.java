78dc4983b900ade84c5c890986d568cd947fc597
Valquiria Matter <matter.valquiria@gmail.com>
2023-03-06 20:57:26 -0300

    teste

--- src/main/java/com/example/aprendendoJPA/models/Cachorro.java
+++ src/main/java/com/example/aprendendoJPA/models/Cachorro.java
@@ -18,7 +18,7 @@ public class Cachorro {
     private String nome;
 
     @Enumerated(EnumType.STRING) //Definindo um ENUM
-    private Raca raca;
+    private Raca racaa;
 
     @ManyToOne //Tornando o relacionamento Bidirecional
     @JoinColumn(name = "pessoa_id")
